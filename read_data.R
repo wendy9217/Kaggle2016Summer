4b5e6e02e0e653fd9b47a05088b0b6fbe7b702a3
Jing Wu <wendy9217@Jings-MacBook-Pro.local>
2016-06-16 21:31:24 -0400

    First commit for EDA R code

---
+++ read_data.R
@@ -0,0 +1,10 @@
+library(data.table)
+
+setwd("~/Dropbox/Kaggle/Code")
+
+dt.sample.submission <- fread("~/Dropbox/Kaggle/Data/sample_submission.csv")
+dt.client.names <- fread("~/Dropbox/Kaggle/Data/cliente_tabla.csv")
+dt.product.names <- fread("~/Dropbox/Kaggle/Data/producto_tabla.csv")
+dt.train <- fread("~/Dropbox/Kaggle/Data/train.csv")
+dt.test <- fread("~/Dropbox/Kaggle/Data/test.csv")
+dt.town.state <- fread("~/Dropbox/Kaggle/Data/town_state.csv")
